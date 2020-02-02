39e35c0880b206ae6b99958e688a40c4c10aa8c8
Asus <Asus@Z>
2020-02-02 14:42:40 +0800

    beta

---
+++ src/activity_1_array/Activity_1_Array.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity_1_array;
+
+/**
+ *
+ * @author Brandon
+ */
+public class Activity_1_Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Array_Act1 ble = new Array_Act1();
+
+        ble.Fill_arr();
+       ble.display();
+        ble.Findmax();
+       ble.Findmin();
+        System.out.println("--------------------------");
+        ble.Searchall();
+       
+        
+    }
+}
